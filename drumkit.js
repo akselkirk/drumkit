37c1f5c247d04b6e3f833fb68e9595398fe5faae
Aksel Kirknes <akselkirknes@gmail.com>
2024-02-23 16:17:08 +0100

    Added comments where it might be convenient.

--- drumkit.js
+++ drumkit.js
@@ -1,14 +1,15 @@
 const beatPressedHandler = (e) => {
     const audio = document.querySelector(`audio[data-key='${e.keyCode}']`);
-    if (!audio) return;
+    if (!audio) return;               //Exist if key does not contain an audio.
     const beat = audio.parentElement;
     audio.currentTime = 0;
     audio.play();
     beat.classList.toggle('playing');
 }
 
+// Function called when playing transition has ended. 
 const resetTransition = function(e){
-    if(e.propertyName !== 'transform'){
+    if(e.propertyName !== 'transform'){ // Exits the function if the event propertyname is not of transform.
         return;
     }
     this.classList.remove('playing');
@@ -16,4 +17,4 @@ const resetTransition = function(e){
 
 const beatKeys = document.querySelectorAll(".beat");
 window.addEventListener('keydown', beatPressedHandler);
-beatKeys.forEach(key => key.addEventListener("transitionend", resetTransition));
+beatKeys.forEach(key => key.addEventListener("transitionend", resetTransition)); 
